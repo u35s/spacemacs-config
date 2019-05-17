1f0685dd4e701714587871f0f4c02dfe08a88f1d
zjt <jintao7080@gmail.com>
2019-05-17 17:12:45 +0800

    macro rename

--- plugins/my_macro.el
+++ plugins/my_macro.el
@@ -1,8 +1,8 @@
-(fset 'calc_table
+(fset 'table_calc
    "Gkkkk\C-c\C-cj\C-c\C-cj\C-c\C-cj\C-c\C-c")
 (fset 'table-new-line
    [?o ?| ?| ?\C-u ?\M-! ?e ?c ?h ?o ?  ?- ?n ?  ?\' backspace ?` ?d ?a ?t ?e ?  ?\' ?+ ?% ?m ?- ?% ?d ?\' ?` return tab])
 
-(spacemacs/set-leader-keys "o t c" 'calc_table)
+(spacemacs/set-leader-keys "o t c" 'table_calc)
 (spacemacs/set-leader-keys "o t n l" 'table-new-line)
 (provide 'my-macro)
